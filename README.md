0679d0a687d2c08387a8fbb0f4517fd2c8866009
Aqil Jaafree <wanaqilre@gmail.com>
2024-10-25 20:45:20 +0800

    feat: contract added

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ### ERC 20 TRAIL Contract Verified
 https://sepolia.basescan.org/address/0xD68fAd32Cf19d2f68A853A2aBCbFc8eBD9704E93
 ### QUIZ Contract Verified
-https://sepolia.basescan.org/address/0x9F901376e91EC162B586059423afF5E0A2744FE2
+https://sepolia.basescan.org/address/0x2aC931290e52Dc533A1ab8B073A4a50638445f5d
 
 ```shell
 npx hardhat help
@@ -27,6 +27,7 @@ npx hardhat run scripts/deploy_trail.js --network base_sepolia
 Verify contract:
 ```shell
 npx hardhat verify --network base_sepolia <contract_address> <public_key_address>
+npx hardhat verify --network base_sepolia 0x2aC931290e52Dc533A1ab8B073A4a50638445f5d 0xD68fAd32Cf19d2f68A853A2aBCbFc8eBD9704E93  0x1AB67c4ac117F3c850D5A93784B7701Cc5816387
 ```
 
 .env:
--- contracts/EnhancedQuizContract.sol
+++ contracts/EnhancedQuizContract.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "./TRAIL.sol"; // Import the TRAIL token contract
+import "./TRAIL.sol";
 
 contract EnhancedQuizContract is Ownable, ReentrancyGuard {
-    TRAIL public trailToken;
+    TRAIL public immutable trailToken;
     
     struct Quiz {
         uint256 id;
@@ -17,17 +17,21 @@ contract EnhancedQuizContract is Ownable, ReentrancyGuard {
     }
     
     struct UserQuizAttempt {
-        bool completed;
         uint16 score;
         uint32 lastAttemptTime;
         uint32 attemptedVersion;
+        bool completed;
     }
     
     mapping(uint256 => Quiz) public quizzes;
     mapping(address => mapping(uint256 => UserQuizAttempt)) public userAttempts;
     
-    event QuizCompleted(address user, uint256 quizId, uint16 score, uint256 reward);
-    event QuizUpdated(uint256 quizId, uint32 newVersion);
+    event QuizCompleted(address indexed user, uint256 indexed quizId, uint16 score, uint256 reward);
+    event QuizUpdated(uint256 indexed quizId, uint32 newVersion);
+    
+    error QuizDoesNotExist();
+    error CooldownPeriodNotOver();
+    error InvalidScore();
     
     constructor(address _trailToken, address initialOwner) Ownable(initialOwner) {
         trailToken = TRAIL(_trailToken);
@@ -39,6 +43,8 @@ contract EnhancedQuizContract is Ownable, ReentrancyGuard {
         uint16 _passingScore, 
         uint32 _cooldownPeriod
     ) external onlyOwner {
+        if (_passingScore == 0 || _passingScore > 100) revert InvalidScore();
+        
         Quiz storage quiz = quizzes[_id];
         if (quiz.id == 0) {
             quiz.id = _id;
@@ -53,16 +59,20 @@ contract EnhancedQuizContract is Ownable, ReentrancyGuard {
     }
     
     function completeQuiz(uint256 _quizId, uint16 _score) external nonReentrant {
+        if (_score > 100) revert InvalidScore();
+        
         Quiz storage quiz = quizzes[_quizId];
-        require(quiz.id != 0, "Quiz does not exist");
+        if (quiz.id == 0) revert QuizDoesNotExist();
         
         UserQuizAttempt storage attempt = userAttempts[msg.sender][_quizId];
-        require(block.timestamp >= attempt.lastAttemptTime + quiz.cooldownPeriod, "Cooldown period not over");
+        if (block.timestamp < attempt.lastAttemptTime + quiz.cooldownPeriod) {
+            revert CooldownPeriodNotOver();
+        }
         
-        attempt.completed = true;
         attempt.score = _score;
         attempt.lastAttemptTime = uint32(block.timestamp);
         attempt.attemptedVersion = quiz.version;
+        attempt.completed = true;
         
         if (_score >= quiz.passingScore) {
             uint256 reward = calculateReward(quiz, _score);
@@ -72,8 +82,8 @@ contract EnhancedQuizContract is Ownable, ReentrancyGuard {
     }
     
     function calculateReward(Quiz memory _quiz, uint16 _score) internal pure returns (uint256) {
-        uint256 scoreFactor = uint256(_score) * 100 / uint256(_quiz.passingScore);
-        return _quiz.baseReward * scoreFactor / 100;
+        // Base reward multiplied by score percentage
+        return (_quiz.baseReward * _score) / 100;
     }
     
     function getUserQuizAttempt(address _user, uint256 _quizId) external view returns (
@@ -83,6 +93,32 @@ contract EnhancedQuizContract is Ownable, ReentrancyGuard {
         uint32 attemptedVersion
     ) {
         UserQuizAttempt memory attempt = userAttempts[_user][_quizId];
-        return (attempt.completed, attempt.score, attempt.lastAttemptTime, attempt.attemptedVersion);
+        return (
+            attempt.completed,
+            attempt.score,
+            attempt.lastAttemptTime,
+            attempt.attemptedVersion
+        );
+    }
+
+    function getQuizDetails(uint256 _quizId) external view returns (
+        uint256 id,
+        uint256 baseReward,
+        uint16 passingScore,
+        uint32 cooldownPeriod,
+        uint32 version
+    ) {
+        Quiz memory quiz = quizzes[_quizId];
+        return (
+            quiz.id,
+            quiz.baseReward,
+            quiz.passingScore,
+            quiz.cooldownPeriod,
+            quiz.version
+        );
+    }
+
+    function isQuizCompleted(address _user, uint256 _quizId) external view returns (bool) {
+        return userAttempts[_user][_quizId].completed;
     }
 }
